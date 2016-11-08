b0de1831f2e7f9df6d471f2e2f41c4f6d5cce096
Jimmy Miller <jimmyhmiller@gmail.com>
2016-11-08 13:11:19 -0500

    Added a higher order constraint to really simplify adding new functions

--- src/runtime/providers/string.ts
+++ src/runtime/providers/string.ts
@@ -5,31 +5,139 @@
 import {Constraint} from "../join";
 import * as providers from "./index";
 
-// Concat strings together. Args expects a set of variables/string constants
-// to concatenate together and an array with a single return variable
-class Concat extends Constraint {
-  // To resolve a proposal, we concatenate our resolved args
-  resolveProposal(proposal, prefix) {
-    let {args} = this.resolve(prefix);
-    return [args.join("")];
-  }
 
-  // We accept a prefix if the return is equivalent to concatentating
-  // all the args
-  test(prefix) {
-    let {args, returns} = this.resolve(prefix);
-    return args.join("") === returns[0];
-  }
+const camelCased = s => s.replace(/-([a-z])/g, g => g[1].toUpperCase());
 
-  // concat always returns cardinality 1
+const argsToAttributeMapping = (args, attributeMapping) => {
+  if (!attributeMapping) {
+    return args;
+  }
+  const obj = {}
+  Object.keys(attributeMapping).forEach(am => {
+    obj[camelCased(am)] = args[attributeMapping[am]];
+  })
+  return obj
+}
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  // If you don't care about the order of the elements inside
+  // the array, you should sort both arrays here.
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+const makeConstraint = f => class Test extends Constraint {
+  static AttributeMapping = f.prototype.AttributeMapping;
+
   getProposal(tripleIndex, proposed, prefix) {
-    let proposal = this.proposalObject;
+    const proposal = this.proposalObject;
+    const { args } = this.resolve(prefix);
+    const objArgs = argsToAttributeMapping(args, f.prototype.AttributeMapping);
     proposal.providing = proposed;
-    proposal.cardinality = 1;
+    const result = f(objArgs);
+    proposal.cardinality = result[0] === false ? 0 : f(objArgs).length;
     return proposal;
+  }
+
+  test(prefix) {
+    const {args, returns} = this.resolve(prefix);
+    const objArgs = argsToAttributeMapping(args, f.prototype.AttributeMapping);
+    return arraysEqual(f(objArgs), returns);
+  }
+
+  resolveProposal(proposal, prefix) {
+    const {args} = this.resolve(prefix);
+    const objArgs = argsToAttributeMapping(args, f.prototype.AttributeMapping);
+    return f(objArgs);
   }
 }
 
+const concat = (args) => [args.join("")];
+const Concat = makeConstraint(concat);
+
+
+const replace = ({ text, subtext, with: withText }) => [text.replace(subtext, withText)];
+replace.prototype.AttributeMapping = {
+  "text": 0,
+  "subtext": 1,
+  "with": 2,
+}
+const Replace = makeConstraint(replace);
+
+
+const length = ({ text }) => [text.length]
+length.prototype.AttributeMapping = {
+    "text": 0,
+ }
+const Length = makeConstraint(length);
+
+const charAt = ({ text, index }) => [text[index]];
+charAt.prototype.AttributeMapping = {
+  text: 0,
+  index: 1,
+}
+const CharAt = makeConstraint(charAt);
+
+const find = ({ text, subtext, caseSensitive }) => {
+  text = !caseSensitive ? text.toLowerCase() : text;
+  subtext = !caseSensitive ? subtext.toLowerCase() : subtext;
+  let temp = [];
+  for (let i = 0; i < text.length; i ++) {
+    if (text.substring(i, i + subtext.length) === subtext) {
+       temp.push(i);
+    }
+  }
+  return temp;
+}
+
+find.prototype.AttributeMapping = {
+  "text": 0,
+  "subtext": 1,
+  "case-sensitive": 2,
+}
+
+const Find = makeConstraint(find);
+
+const substring = ({ text, from, to }) => [text.substring(from, to)];
+substring.prototype.AttributeMapping = {
+  "text": 0,
+  "from": 1,
+  "to": 2,
+}
+const Substring = makeConstraint(substring); 
+
+const lower = ({ text }) => [text.toLowerCase()];
+lower.prototype.AttributeMapping = {
+  "text": 0,
+}
+const Lower = makeConstraint(lower);
+
+const upper = ({ text }) => [text.toUpperCase()];
+upper.prototype.AttributeMapping = {
+  "text": 0,
+}
+const Upper = makeConstraint(upper);
+
+const startsWith = ({ text, with: withText }) => [text.startsWith(withText)];
+startsWith.prototype.AttributeMapping = {
+  "text": 0,
+  "with": 1,
+}
+const StartsWith = makeConstraint(startsWith);
+
+
+const trim = ({ text, with: withText }) => [text.trim()];
+trim.prototype.AttributeMapping = {
+  "text": 0,
+}
+const Trim = makeConstraint(trim);
 
 
 class Split extends Constraint {
@@ -100,211 +208,6 @@ class Split extends Constraint {
 }
 
 
-class Replace extends Constraint {
-  static AttributeMapping = {
-    "text": 0,
-    "subtext": 1,
-    "with": 2,
-  }
-
-  resolveProposal(proposal, prefix) {
-    let {args, returns} = this.resolve(prefix);
-    let [text, subtext, _with] = args; // with is a reserved word
-    return [text.replace(subtext, _with)];
-  }
-
-  test(prefix) {
-    let {args, returns} = this.resolve(prefix);
-    let [text, subtext, _with] = args;
-    if(typeof text !== "string") return false;
-    return text.replace(subtext, _with) === returns[0];
-  }
-
-  getProposal(tripleIndex, proposed, prefix) {
-    let proposal = this.proposalObject;
-    let {args} = this.resolve(prefix);
-    if(typeof args[0] !== "string") {
-      proposal.cardinality = 0;
-    } else {
-      proposal.providing = proposed;
-      proposal.cardinality = 1;
-    }
-    return proposal;
-  }
-}
-
-class Length extends Constraint {
-  static AttributeMapping = {
-    "text": 0,
-  }
-  resolveProposal(proposal, prefix) {
-    let {args} = this.resolve(prefix);
-    let [text] = args;
-    return [text.length];
-  }
-
-  test(prefix) {
-    let {args, returns} = this.resolve(prefix);
-    let text = args[0];
-    if(typeof text !== "string") return false;
-    return text.length === returns[0];
-  }
-
-  getProposal(tripleIndex, proposed, prefix) {
-    let proposal = this.proposalObject;
-    let {args} = this.resolve(prefix);
-    if(typeof args[0] !== "string") {
-      proposal.cardinality = 0;
-    } else {
-      proposal.providing = proposed;
-      proposal.cardinality = 1;
-    }
-    return proposal;
-  }
-}
-
-
-class CharAt extends Constraint {
-  static AttributeMapping = {
-    "text": 0,
-    "index": 1,
-  }
-  resolveProposal(proposal, prefix) {
-    let {args} = this.resolve(prefix);
-    let [text, index] = args;
-    return [text[index]];
-  }
-
-  test(prefix) {
-    let {args, returns} = this.resolve(prefix);
-    let [text, index] = args;
-    if(typeof text !== "string") return false;
-    if(index < 0 || index >= text.length) return false;
-    return text[index] === returns[0];
-  }
-
-  getProposal(tripleIndex, proposed, prefix) {
-    let proposal = this.proposalObject;
-    let {args, returns} = this.resolve(prefix);
-    let [text, index] = args;
-    if(typeof text !== "string" || index < 0 || index >= text.length) {
-      proposal.cardinality = 0;
-    } else {
-      proposal.providing = proposed;
-      proposal.cardinality = 1;
-    }
-    return proposal;
-  }
-}
-
-
-class Find extends Constraint {
-  static AttributeMapping = {
-    "text": 0,
-    "subtext": 1,
-    "case-sensitive": 2,
-  }
-
-  findAll(text, subtext, caseSensitive) {
-    text = caseSensitive ? text.toLowerCase() : text;
-    subtext = caseSensitive ? subtext.toLowerCase() : subtext;
-    let temp = [];
-    for (let i = 0; i < text.length; i ++) {
-      if (text.substring(i, i + subtext.length) === subtext) {
-         temp.push(i);
-      }
-    }
-    return temp;
-  }
-
-  testAll(text, subtext, indexes, caseSensitive) {
-    text = caseSensitive ? text.toLowerCase() : text;
-    subtext = caseSensitive ? subtext.toLowerCase() : subtext;
-    for (let i of indexes) {
-      if (i < 0 || i > text.length || text.substring(i, i + subtext.length) !== subtext) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  resolveProposal(proposal, prefix) {
-    let {args} = this.resolve(prefix);
-    let [text, subtext, caseSensitive=true] = args;
-    return this.findAll(text, subtext, caseSensitive);
-  }
-
-  test(prefix) {
-    let {args, returns} = this.resolve(prefix);
-    let [text, subtext, caseSensitive=true] = args;
-    console.log('test', returns)
-    let [indexes] = returns;
-    if(typeof text !== "string") return false;
-    return this.testAll(text, subtext, indexes, caseSensitive);
-  }
-
-  getProposal(tripleIndex, proposed, prefix) {
-    let proposal = this.proposalObject;
-    let {args,} = this.resolve(prefix);
-    let [text, subtext, caseSensitive=true] = args;
-    if(typeof text !== "string") {
-      proposal.cardinality = 0;
-    } else {
-      proposal.providing = proposed;
-      proposal.cardinality = this.findAll(text, subtext, caseSensitive).length;
-    }
-    return proposal;
-  }
-}
-
-// substring over the field 'text', with the base index being 1, inclusive, 'from' defaulting
-// to the beginning of the string, and 'to' the end
-class Substring extends Constraint {
-  static AttributeMapping = {
-    "text": 0,
-    "from": 1,
-    "to": 2,
-  }
-  static ReturnMapping = {
-    "value": 0,
-  }
-  // To resolve a proposal, we concatenate our resolved args
-  resolveProposal(proposal, prefix) {
-    let {args, returns} = this.resolve(prefix);
-    let from = 0;
-    let text = args[0];
-    let to = text.length;
-    if (args[1] != undefined) from = args[1] - 1;
-    if (args[2] != undefined) to = args[2];
-    return [text.substring(from, to)];
-  }
-
-  test(prefix) {
-    let {args, returns} = this.resolve(prefix);
-    let from = 0;
-    let text = args[0];
-    if(typeof text !== "string") return false;
-    let to = text.length;
-    if (args[1] != undefined) from = args[1] - 1;
-    if (args[2] != undefined) to = args[2];
-    console.log("test string", text.substring(from, to), from, to, returns[0]);
-    return text.substring(from, to) === returns[0];
-  }
-
-  // substring always returns cardinality 1
-  getProposal(tripleIndex, proposed, prefix) {
-    let proposal = this.proposalObject;
-    let {args} = this.resolve(prefix);
-    if(typeof args[0] !== "string") {
-      proposal.cardinality = 0;
-    } else {
-      proposal.providing = proposed;
-      proposal.cardinality = 1;
-    }
-    return proposal;
-  }
-}
-
 class Convert extends Constraint {
   static AttributeMapping = {
     "value": 0,
@@ -370,6 +273,10 @@ class Convert extends Constraint {
   }
 }
 
+providers.provide("trim", Trim);
+providers.provide("starts-with", StartsWith);
+providers.provide("upper", Upper);
+providers.provide("lower", Lower);
 providers.provide("find", Find);
 providers.provide("char-at", CharAt);
 providers.provide("replace", Replace);
